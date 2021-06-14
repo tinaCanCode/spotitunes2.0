9cfb675d6334e97d36f9726ac53abdada7f0221c
tinaCanCode <mc.scherzberg@googlemail.com>
2021-06-14 08:25:35 +0200

    clean view files from unnecessary comments, remove rating and comments feature from views

--- app.js
+++ app.js
@@ -8,28 +8,9 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
-const unirest = require('unirest');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-
-//require spotify Web api
 const SpotifyWebApi = require('spotify-web-api-node');
-// required setup for listennotes api
-
-// const response = await unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0')
-// .header('X-ListenAPI-Key', process.env.LISTENNOTES_APIKEY)
-// response.toJSON();
-
-let result 
-unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0')
-  .header('X-ListenAPI-Key', process.env.LISTENNOTES_APIKEY).then(response=>{
-    //console.log("response from api",response.toJSON())
-    result = response.toJSON()
-  })
-//response.toJSON();
-
-
-
 
 
 mongoose
@@ -66,7 +47,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
@@ -88,22 +68,16 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 hbs.registerPartials(__dirname + "/views/partials");
 
-
-
-
-// default value for title local
-// app.locals.title = 'Spotitunes - Find all your favorite Podcasts in one App';
+// App title
 app.locals.title = 'Spotitunes';
 
-
-
+// Routing setup
 const index = require('./routes/index');
 app.use('/', index);
 
